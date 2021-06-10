b059e1fe3684c7f53891c996552c238da2834f38
RikkaMoe <bayuwidiastika@gmail.com>
2021-06-10 15:31:39 +0800

    Fix Delete Properties Owner and Agent

--- heraGeneral/core/page/agent/delete.php
+++ heraGeneral/core/page/agent/delete.php
@@ -7,12 +7,17 @@
 	$query = mysqli_query($conn, $cekharga);
 	$data = mysqli_fetch_array($query);
 	$id_properties = $data['id_properties'];
-
-	$perintah = "UPDATE tb_management_agent SET status_management_agent = '-1', updated_at = '$wktUpdate' WHERE id_management_agent = '$id'";
-	$perintah2 = "UPDATE tb_properties SET price = '', status = '1', updated_at = '$wktUpdate' WHERE id_properties = '$id_properties'";
+	$status_management = $data['status_management_agent'];
 	
-	mysqli_query($conn, $perintah);
-	mysqli_query($conn, $perintah2);
+	if ($status_management == 2) {
+		echo " <script>alert('Gagal, Properti ini sudah dibeli oleh customer !');window.location='?page=agent&aksi=management'; </script> ";
+	} else {
+		$perintah = "UPDATE tb_management_agent SET status_management_agent = '-1', updated_at = '$wktUpdate' WHERE id_management_agent = '$id'";
+		$perintah2 = "UPDATE tb_properties SET price = '', status = '1', updated_at = '$wktUpdate' WHERE id_properties = '$id_properties'";
+	
+		mysqli_query($conn, $perintah);
+		mysqli_query($conn, $perintah2);
 
-	header("location:?page=agent&aksi=management");
+		header("location:?page=agent&aksi=management");
+	}
 ?>
--- heraGeneral/core/page/owner/delete.php
+++ heraGeneral/core/page/owner/delete.php
@@ -1,6 +1,18 @@
 <?php 
 	$id = $_GET['id'];
-	$perintah = "DELETE FROM tb_properties WHERE id_properties = '$id'";
-	mysqli_query($conn, $perintah);
-	header("location:?page=owner&aksi=management");
+	$perintah = "SELECT * FROM tb_properties WHERE id_properties = '$id'";
+	$query = mysqli_query($conn, $perintah);
+	$data = mysqli_fetch_array($query);
+
+	$status = $data['status'];
+
+	if ($status == 3){
+		echo " <script>alert('Gagal, Properti ini sudah dibeli oleh customer !');window.location='?page=owner&aksi=management'; </script> ";
+	} else if ($status == 2) {
+		echo " <script>alert('Gagal, Properti ini sudah dibeli oleh customer !');window.location='?page=owner&aksi=management'; </script> ";
+	} else {
+		$perintah2 = "DELETE FROM tb_properties WHERE id_properties = '$id'";
+		mysqli_query($conn, $perintah2);
+		header("location:?page=owner&aksi=management");
+	}
 ?>
